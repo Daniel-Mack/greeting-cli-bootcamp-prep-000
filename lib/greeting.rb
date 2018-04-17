fa25f012e4762b98db81d30f0e115aac11a2d83a
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-17 21:34:44 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -3,4 +3,4 @@ def greeting(name)
   puts "Hello!"
   puts "Please enter your name so we can address you properly."
   name = gets.strip
-  puts "Nice to meet you, #{{name}}!"
+  puts "Nice to meet you, #{{name}!"
