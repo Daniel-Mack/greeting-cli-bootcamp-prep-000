4418eba9d153818248b71e29d1a46e374d657b52
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-17 21:43:48 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,9 +1,4 @@
 # code the #greeting method here!
 def greeting(name)
-  puts "Hello!"
-  puts "Please enter your name so we can address you properly."
-  name = gets.strip
   puts "Nice to meet you, #{name}!"
 end
-
-greeting(name)
